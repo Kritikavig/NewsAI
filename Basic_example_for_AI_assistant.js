fc470a74c58633686a06ddd4b178e286a690264b
Kritika <83600842+Kritikavig@users.noreply.github.com>
2023-08-01 00:47:26 +0530

    script added

---
+++ Basic_example_for_AI_assistant.js
@@ -0,0 +1,184 @@
+
+
+intent('What does this app do?','What can I do here?',
+reply('This is a news project'))
+
+const API_KEY = '31f1750303a44ee1a2112309776e667b';
+let savedArticles = [];
+
+// News by Source, 'Give me the news from $(source* (.*))'
+intent('(give me | tell me | show me) (latest|recent|) news (from | by | on | in)  $(source* (.*))', (p) => {
+let NEWS_API_URL = `https://newsapi.org/v2/top-headlines`;
+
+if(p.source.value) {
+    p.source.value=p.source.value.toLowerCase().split(" ").join("-");
+    NEWS_API_URL = `${NEWS_API_URL}?sources=${p.source.value}&apiKey=${API_KEY}`
+}
+
+api.request(NEWS_API_URL, {headers: {"user-agent": 'user agent' }}, (error, response, body) => {
+    const { totalResults, articles } = JSON.parse(body);
+
+    if(totalResults == 0) {
+        p.play('Sorry, please try searching for news from a different source');
+        return;
+    }
+
+    savedArticles = articles;
+
+    p.play({ command: 'newHeadlines', articles });
+    p.play(`Here are the (latest|recent) ${p.source.value}.`);
+
+    p.play('Would you like me to read the headlines?');
+    p.then(confirm);
+    });
+})
+
+intent('give information about $(term* (.*))', (p) => {
+//let NEWS_API_URL = `https://newsapi.org/v2/everything?apiKey=${API_KEY}`; 
+ let NEWS_API_URL = 'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=31f1750303a44ee1a2112309776e667b';
+if(p.term.value) {
+    NEWS_API_URL = `${NEWS_API_URL}&q=${p.term.value}`
+}
+
+api.axios.get(NEWS_API_URL).then((response)=>{
+    let {articles} = response.data;
+    
+    if(!articles.length) {
+        p.play('Sorry, please try searching for something else.');
+        return;
+    }
+    
+    savedArticles = articles;
+    
+    p.play({ command: 'newsHeadlines', articles });
+    p.play(`Here are the (latest|recent) articles on ${p.term.value}.`);
+    
+    p.play('Would you like me to read the headlines?');
+    p.then(confirm);
+});
+})
+
+const confirm = context(() => {
+    intent('yes', async (p) => {
+        for (let i = 0; i < savedArticles.length; i++) {
+            p.play({ command: 'highlight', article: savedArticles[i] });
+            p.play(`${savedArticles[i].title}`);
+        }
+    })
+
+    intent('no', (p) => {
+        p.play('Sure, sounds good to me.');
+    })
+})
+
+
+// const API_KEY='31f1750303a44ee1a2112309776e667b';
+// let savedArticles = [];
+// //News By Source
+// 
+// intent('Give me the news from $(source* (.*))',(p) => {
+// 
+// let NEWS_API_URL = 'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=31f1750303a44ee1a2112309776e667b';
+// 
+// if(p.source.value) {
+//     NEWS_API_URL = `${NEWS_API_URL}&sources=${p.source.value.toLowerCase().split(" ").join('-')}`
+// }
+// 
+// api.axios.get(NEWS_API_URL).then((response)=>{
+//     let {articles} = response.data;
+//     if(!articles.length){
+//         p.play('Sorry, please try searching for news from a different source');
+//         return;
+//     }
+//     savedArticles = articles;
+//     p.play({command: 'newsHeadlines', articles});
+//     p.play(`Here are the (latest|recent) ${p.source.value}`);
+//     
+//     p.play('Would you like me to read the headlines?');
+//     p.then(confirmation);
+// })
+// });
+// // News by Term
+// intent('give information about $(term* (.*))', (p) => {
+// let NEWS_API_URL = `https://newsapi.org/v2/everything?apiKey=${API_KEY}`; 
+// 
+// if(p.term.value) {
+//     NEWS_API_URL = `${NEWS_API_URL}&q=${p.term.value}`
+// }
+// 
+// api.axios.get(NEWS_API_URL).then((response)=>{
+//     let {articles} = response.data;
+//     
+//     if(!articles.length) {
+//         p.play('Sorry, please try searching for something else.');
+//         return;
+//     }
+//     
+//     savedArticles = articles;
+//     
+//     p.play({ command: 'newsHeadlines', articles });
+//     p.play(`Here are the (latest|recent) articles on ${p.term.value}.`);
+//     
+//     p.play('Would you like me to read the headlines?');
+//     p.then(confirmation);
+// });
+// })
+// 
+// // News by Categories
+// const CATEGORIES = ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology'];
+// const CATEGORIES_INTENT = `${CATEGORIES.map((category) => `${category}~${category}`).join('|')}`;
+// 
+// intent(`(show|what is|tell me|what's|what are|what're|read) (the|) (recent|latest|) $(N news|headlines) (in|about|on|) $(C~ ${CATEGORIES_INTENT})`,
+//   `(read|show|get|bring me|give me) (the|) (recent|latest) $(C~ ${CATEGORIES_INTENT}) $(N news|headlines)`, (p) => {
+//     let NEWS_API_URL = `https://newsapi.org/v2/top-headlines?apiKey=${API_KEY}&country=us`;
+//     
+//     if(p.C.value) {
+//         NEWS_API_URL = `${NEWS_API_URL}&category=${p.C.value}`;
+//     }
+//     
+//     api.request(NEWS_API_URL, (error, response, body) => {
+//         const { articles } = JSON.parse(body);
+//         
+//         if(!articles.length) {
+//             p.play('Sorry, please try searching for a different category.');
+//             return;
+//         }
+//         
+//         savedArticles = articles;
+//         
+//         p.play({ command: 'newHeadlines', articles });
+//         
+//         if(p.C.value) {
+//             p.play(`Here are the (latest|recent) articles on ${p.C.value}.`);        
+//         } else {
+//             p.play(`Here are the (latest|recent) news`);   
+//         }
+//         
+//         p.play('Would you like me to read the headlines?');
+//         p.then(confirmation);
+//     });
+// });
+// 
+// const confirmation = context(() => {
+//     intent('yes', async (p) => {
+//         for(let i = 0; i < savedArticles.length; i++){
+//             p.play({ command: 'highlight', article: savedArticles[i]});
+//             p.play(`${savedArticles[i].title}`);
+//         }
+//     })
+//     
+//     intent('no', (p) => {
+//         p.play('Sure, sounds good to me.')
+//     })
+// })
+// 
+// intent('open (the|) (article|) (number|) $(number* (.*))', (p) => {
+//     if(p.number.value) {
+//         p.play({ command:'open', number: p.number.value, articles: savedArticles})
+//     }
+// })
+// 
+// intent('(go|) back', (p) => {
+//     p.play('Sure, going back');
+//     p.play({ command: 'newHeadlines', articles: []})
+// })
